1a9a60d4a027b66256a48c86c97ba31b40507d2b
mrzephyr17 <pedromiguel.fernandes17@gmail.com>
2019-04-12 11:28:30 +0100

    improved start script

--- T1/scripts/start.sh
+++ T1/scripts/start.sh
@@ -2,19 +2,13 @@
 
 version=$1
 numPeers=$2
-invert=$3
+start=$3
 
 if [ $# -ne 3 ]; then
-    echo "Usage: $0 <version> <number of peers> <invert>"
+    echo "Usage: $0 <version> <number of peers> <start>"
     exit 1
 fi
 
-if [ $invert = "true" ]; then
-    for ((i = numPeers ; i > 0 ; i--)); do
-        gnome-terminal -x sh -c "java Peer $version $i hello 224.0.0.0 8001 224.0.0.1 8081 224.0.0.2 8082"
-    done
-else
-    for ((i = 1 ; i <= numPeers ; i++)); do
-        gnome-terminal -x sh -c "java Peer $version $i hello 224.0.0.0 8001 224.0.0.1 8081 224.0.0.2 8082"
-    done
-fi
+for ((i = $start ; i < $start + numPeers ; i++)); do
+    gnome-terminal -x sh -c "java Peer $version $i hello 224.0.0.0 8001 224.0.0.1 8081 224.0.0.2 8082"
+done
